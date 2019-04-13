2ac25a06641e4fee4038445b2dcba2b7a89e0f3d
Dominic DiTaranto <domdit@gmail.com>
2019-04-12 21:29:42 -0700

    setting up a blog Phase 2

--- __init__.py
+++ __init__.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_recaptcha import ReCaptcha
-from flask_blogging import SQLAStorage, BloggingEngine
 
 
 import os
